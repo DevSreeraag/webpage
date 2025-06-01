00e3297b28e10ed49805b28729652e96b8d0be3a
Shahimbadi <shahimbadi08@gmail.com>
2025-06-01 22:57:09 +0530

    Your commit message

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Our Web',
+      title: 'Our Website',
       theme: ThemeData(
         // This is the theme of your application.
         //
